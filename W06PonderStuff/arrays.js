ebb6caf4270a28aaf77cda66d640ef090cfc90eb
TauntingRhinomonster <90732061+TauntingRhinomonster@users.noreply.github.com>
2025-05-30 11:32:51 -0600

    I finished the arrays.js ponder assignment!

--- W06PonderStuff/arrays.js
+++ W06PonderStuff/arrays.js
@@ -22,18 +22,32 @@ function convertToGpa(grade) {
     return points;
 }
 
-// I am on part 4 of the ponder activity
-// function getGpaPoints(list) {
-    
-// }
-
 
 let newArray = steps.map(listTemplate);
 let gpaList = grades.map(convertToGpa);
 const formattedList = newArray.join('');
+// Reduce
+const reduced = gpaList.reduce((total, item) => total + item);
+const gpa = reduced / gpaList.length;
+// Filter
+let fruit = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+const filteredFruit = fruit.filter(fruit => fruit.length < 6);
+// indedOf
+const numbers = [12, 34, 21, 54];
+const luckNumber = 21;
+let index = numbers.indexOf(luckNumber);
 
 
-console.log(gpaList);
-console.log(formattedList);
 const stepsHtml = // use map to convert the list from strings to HTML
 document.querySelector("#myList").innerHTML = formattedList;
+
+
+// displaying data on the console
+console.log(gpaList);
+console.log(formattedList);
+console.log(gpa);
+console.log(`Unfiltered: ${fruit}`);
+console.log(`Filtered: ${filteredFruit}`);
+console.log(`This is my list of numbers: ${numbers}`);
+console.log(`My lucky number is ${luckNumber}`);
+console.log(`The lucky number 21 was found at index: ${index}`);
